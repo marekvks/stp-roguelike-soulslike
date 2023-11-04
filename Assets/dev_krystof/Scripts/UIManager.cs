d31e594c7df8224a639c8fb89d93f96faffe66c6
Krystof Jezdik <jezdik.kr.2021@skola.ssps.cz>
2023-11-04 20:41:17 +0100

    feat: Main Menu
    Dokončení základního hlavního menu. Přidání měnění náhledového modelu při vytváření nové hry. Možnost otáčení náhledového modelu.

--- Assets/dev_krystof/Scripts/UIManager.cs
+++ Assets/dev_krystof/Scripts/UIManager.cs
@@ -10,33 +10,41 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Image _backgroundRenderer;
     [SerializeField] private Color _color1;
     [SerializeField] private Color _color2;
-    [SerializeField] private Transform _characterX;
-    [SerializeField] private Transform _characterY;
+    [SerializeField] private Transform _character;
     [SerializeField] private float _rotationSpeed;
-    [SerializeField] private Transform _finalRot;
+    [SerializeField] private Transform _axis;
  
     private void Update()
     {
+        // Smoothly fade background between given colours
         _lerpColour = Color.Lerp(_color1, _color2, Mathf.PingPong(Time.time, 1));
         _backgroundRenderer.color = _lerpColour;
         
         RotateWithChar();
     }
 
+    /// <summary>
+    /// Rotating preview model 
+    /// </summary>
     void RotateWithChar()
     {
         if (Input.GetKey(KeyCode.Mouse0))
         {
+            // Getting input
             float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * _rotationSpeed;
-            float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * _rotationSpeed;
 
-            Vector3 dirX = new Vector3(0, -mouseX, 0);
-            Vector3 dirY = new Vector3(mouseY, 0, 0);
-
-            _characterX.eulerAngles += dirX;
-            _characterY.eulerAngles += dirY;
-
-            _finalRot.eulerAngles = _characterX.rotation.eulerAngles + _characterY.rotation.eulerAngles;
+            //Creating Vector3
+            Vector3 dir = new Vector3(0, -mouseX, 0);
+            //Rotating preview model around X axis of parent by mouse
+            _character.RotateAround(_axis.position, dir, dir.magnitude * _rotationSpeed);
         }
+    }
+
+    /// <summary>
+    ///  Function for quiting aplication
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
