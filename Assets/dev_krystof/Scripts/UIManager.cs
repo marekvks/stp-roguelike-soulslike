4e89e7698dd57c5e5d0db00ccf91f9c8025a5363
Krystof Jezdik <jezdik.kr.2021@skola.ssps.cz>
2023-11-15 20:42:57 +0100

    feat: New Input System
    Změna na New Input System, nic jiného se nemění.

--- Assets/dev_krystof/Scripts/UIManager.cs
+++ Assets/dev_krystof/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class UIManager : MonoBehaviour
 {
@@ -13,13 +14,23 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Transform _character;
     [SerializeField] private float _rotationSpeed;
     [SerializeField] private Transform _axis;
- 
+    private MainMenuControls _controls;
+    private MainMenuControls.MainMenuActions _mainMenuActions;
+    private float mouseX;
+    [SerializeField] private bool _inNewGameMenu = false;
+
+    private void Awake()
+    {
+        _controls = new MainMenuControls();
+        _mainMenuActions = _controls.MainMenu;
+        _mainMenuActions.RotatingModel.performed += ctx => mouseX = ctx.ReadValue<float>();
+    }
+
     private void Update()
     {
         // Smoothly fade background between given colours
         _lerpColour = Color.Lerp(_color1, _color2, Mathf.PingPong(Time.time, 1));
         _backgroundRenderer.color = _lerpColour;
-        
         RotateWithChar();
     }
 
@@ -28,16 +39,17 @@ public class UIManager : MonoBehaviour
     /// </summary>
     void RotateWithChar()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            // Getting input
-            float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * _rotationSpeed;
-
-            //Creating Vector3
-            Vector3 dir = new Vector3(0, -mouseX, 0);
-            //Rotating preview model around X axis of parent by mouse
-            _character.RotateAround(_axis.position, dir, dir.magnitude * _rotationSpeed);
-        }
+        // Getting input
+            //mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * _rotationSpeed;
+            if (_mainMenuActions.MouseButtonClicked.ReadValue<float>() > 0.1 && _inNewGameMenu)
+            {
+                mouseX *= Time.deltaTime * _rotationSpeed;
+            
+                //Creating Vector3
+                Vector3 dir = new Vector3(0, -mouseX, 0);
+                //Rotating preview model around X axis of parent by mouse
+                _character.RotateAround(_axis.position, dir, dir.magnitude * _rotationSpeed);    
+            }
     }
 
     /// <summary>
@@ -46,5 +58,25 @@ public class UIManager : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void NewGameMenuOpen()
+    {
+        _inNewGameMenu = true;
+    }
+
+    public void NewGameMenuClose()
+    {
+        _inNewGameMenu = false;
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
     }
 }
