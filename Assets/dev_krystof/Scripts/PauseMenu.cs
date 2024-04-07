8c25350a67830e72e7419a5bdf4bd08dfe42db09
Kryštof <jezdikk@gmail.com>
2024-04-07 19:57:37 +0200

    feat: UI Final (design)

---
+++ Assets/dev_krystof/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _pauseMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        _pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        _pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
